08f17baddf66247faf7fa260e0f71d9a5be80401
Jiri Zejda <me@jirizejda.cz>
2022-10-27 01:25:40 +0200

    Add Dashboard widgets.

---
+++ app/Filament/Resources/PostResource/Widgets/PostsOverview.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Resources\PostResource\Widgets;
+
+use Filament\Widgets\Widget;
+
+class PostsOverview extends Widget
+{
+    protected static string $view = 'filament.resources.post-resource.widgets.posts-overview';
+}
---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Widgets;
+
+use App\Models\Post;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use Illuminate\Support\Facades\DB;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        return [
+            Card::make('Unique views', '192.1k'),
+            Card::make('Bounce rate', '21%'),
+            Card::make('Average time on page', $this->getLastPost()),
+        ];
+    }
+
+    private function getLastPost(): string
+    {
+        /**
+         * @var Post $lastPrivatePost
+         */
+        $lastPrivatePost = DB::table('posts')
+            ->where('private', '=', 1)
+            ->first();
+
+        if(!is_null($lastPrivatePost)) {
+            return $lastPrivatePost->content;
+        } else {
+            return '';
+        }
+    }
+}
