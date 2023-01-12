c294d3117f85da640bded303a6bb0543ecae2130
Jiri Zejda <me@jirizejda.cz>
2023-01-12 22:45:37 +0100

    Update Dashboard stats

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Filament\Widgets;
 
 use App\Models\Post;
+use Auth;
 use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
+use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 use Illuminate\Support\Facades\DB;
@@ -16,10 +18,25 @@ class StatsOverview extends BaseWidget
 
     protected function getCards(): array
     {
+        $date = Carbon::now();
+
+        $startOfYear = $date->copy()->startOfYear();
+        $endOfYear   = $date->copy()->endOfYear();
+
+        $usersCount = DB::table('users')->count();
+        $userProfilesCount = DB::table('user_race_profiles')->count();
+        $sportEventsCount = DB::table('sport_events')
+            ->where('date', '>', $startOfYear)
+            ->where('date', '<', $endOfYear)
+            ->count();
+
+        $jo = Auth::user()->hasRole('super_admin');
+
         return [
-            Card::make('Unique views', '192.1k'),
-            Card::make('Bounce rate', '21%'),
-            Card::make('Average time on page', $this->getLastPost()),
+            Card::make('Uživatelů', $usersCount),
+            Card::make('test', $jo),
+            Card::make('Registrace', $userProfilesCount),
+            Card::make('Závodu v roce ' . $date->format('Y'), $sportEventsCount),
         ];
     }
 
