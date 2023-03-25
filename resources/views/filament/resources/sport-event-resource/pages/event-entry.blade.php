32c31f9ea2f0a5e7c5d3ed3a9a7ecdb051ff3f77
Jiri Zejda <me@jirizejda.cz>
2023-03-25 01:01:19 +0100

    Delete unused buttons.

--- resources/views/filament/resources/sport-event-resource/pages/event-entry.blade.php
+++ resources/views/filament/resources/sport-event-resource/pages/event-entry.blade.php
@@ -46,15 +46,15 @@
     <form wire:submit.prevent="submit" class="space-y-6">
         {{ $this->form }}
 
-        <div class="flex flex-wrap items-center gap-4 justify-start">
-            <x-filament::button type="submit">
-                Načti ORIS
-            </x-filament::button>
+{{--        <div class="flex flex-wrap items-center gap-4 justify-start">--}}
+{{--            <x-filament::button type="submit">--}}
+{{--                Načti ORIS 999--}}
+{{--            </x-filament::button>--}}
 
-            <x-filament::button type="button" color="secondary" tag="a" :href="$this->back_button_url">
-                Zpět
-            </x-filament::button>
-        </div>
+{{--            <x-filament::button type="button" color="secondary" tag="a" :href="$this->back_button_url">--}}
+{{--                Zpět--}}
+{{--            </x-filament::button>--}}
+{{--        </div>--}}
     </form>
 
     <div>
