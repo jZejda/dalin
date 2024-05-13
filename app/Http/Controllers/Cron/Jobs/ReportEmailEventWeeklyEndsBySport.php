7406f7ba7cf049e3e292fc494573d9fefb564ae6
Jirka <me@jirizejda.cz>
2024-05-13 17:46:47 +0200

    Update project eventWeeklyEndsBySport blade email

--- app/Http/Controllers/Cron/Jobs/ReportEmailEventWeeklyEndsBySport.php
+++ app/Http/Controllers/Cron/Jobs/ReportEmailEventWeeklyEndsBySport.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Cron\Jobs;
 
-use Illuminate\Support\Facades\DB;
 use App\Mail\EventWeeklyEndsBySport;
 use App\Models\User;
 use App\Shared\Helpers\AppHelper;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
@@ -29,24 +29,24 @@ class ReportEmailEventWeeklyEndsBySport implements CommonCronJobs
                 $eventFirstDateEnd = DB::table('sport_events')
                     ->wherein('sport_id', $user->getUserOptions()['week_report_by_sport'])
                     ->whereNotNull('entry_date_1')
-                    ->where('entry_date_1', '>=', Carbon::now()->addDay()->format(AppHelper::DB_DATE_TIME) . ' 00:00:00')
-                    ->where('entry_date_1', '<=', Carbon::now()->addDays(8)->format(AppHelper::DB_DATE_TIME) . ' 23:59:59')
+                    ->where('entry_date_1', '>=', Carbon::now()->addDay()->format(AppHelper::DB_DATE_TIME).' 00:00:00')
+                    ->where('entry_date_1', '<=', Carbon::now()->addDays(8)->format(AppHelper::DB_DATE_TIME).' 23:59:59')
                     ->orderBy('entry_date_1', 'asc')
                     ->get();
 
                 $eventSecondDateEnd = DB::table('sport_events')
                     ->wherein('sport_id', $user->getUserOptions()['week_report_by_sport'])
                     ->whereNotNull('entry_date_2')
-                    ->where('entry_date_2', '>=', Carbon::now()->addDay()->format(AppHelper::DB_DATE_TIME) . ' 00:00:00')
-                    ->where('entry_date_2', '<=', Carbon::now()->addDays(8)->format(AppHelper::DB_DATE_TIME) . ' 23:59:59')
+                    ->where('entry_date_2', '>=', Carbon::now()->addDay()->format(AppHelper::DB_DATE_TIME).' 00:00:00')
+                    ->where('entry_date_2', '<=', Carbon::now()->addDays(8)->format(AppHelper::DB_DATE_TIME).' 23:59:59')
                     ->orderBy('entry_date_2', 'asc')
                     ->get();
 
                 $eventThirdDateEnd = DB::table('sport_events')
                     ->wherein('sport_id', $user->getUserOptions()['week_report_by_sport'])
                     ->whereNotNull('entry_date_3')
-                    ->where('entry_date_3', '>=', Carbon::now()->addDay()->format(AppHelper::DB_DATE_TIME) . ' 00:00:00')
-                    ->where('entry_date_3', '<=', Carbon::now()->addDays(8)->format(AppHelper::DB_DATE_TIME) . ' 23:59:59')
+                    ->where('entry_date_3', '>=', Carbon::now()->addDay()->format(AppHelper::DB_DATE_TIME).' 00:00:00')
+                    ->where('entry_date_3', '<=', Carbon::now()->addDays(8)->format(AppHelper::DB_DATE_TIME).' 23:59:59')
                     ->orderBy('entry_date_3', 'asc')
                     ->get();
 
@@ -54,7 +54,7 @@ class ReportEmailEventWeeklyEndsBySport implements CommonCronJobs
 
                     Mail::to($user)
                         ->queue(new EventWeeklyEndsBySport($eventFirstDateEnd, $eventSecondDateEnd, $eventThirdDateEnd));
-                    Log::channel('site')->info('MailWeeklyUserEventSummary mail for user: ' . $user->email . ' - ' . $user->name);
+                    Log::channel('site')->info('MailWeeklyUserEventSummary mail for user: '.$user->email.' - '.$user->name);
                 }
             }
         }
--- resources/views/emails/event/eventWeeklyEndsBySport.blade.php
+++ resources/views/emails/event/eventWeeklyEndsBySport.blade.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Str;
 
 ## Konec přihlášek
 
-Blíží ze konec přihlášek na závody vypasané níže. Závody mají konec přihlášek následujících termínů v seznamu níže v rámci příštího týdne
-od **{{ Carbon::now()->addDay()->format('d.m.Y') }}** do **{{ Carbon::now()->addDays(8)->format('d.m.Y') }}**.
+Týdenní souhrn přihlášek závodů vypsaných níže. Závody jsou rozděleny podle termínu přihlášek v týdnu
+ **{{ Carbon::now()->addDay()->format('d.m.Y') }}** - **{{ Carbon::now()->addDays(8)->format('d.m.Y') }}**.
 
 @if(!is_null($eventFirstDateEnd))
 @component('mail::divider')
@@ -26,11 +26,11 @@ Závody u kterých končí **první termín** přihlášek.
 @endcomponent
 
 @component('mail::table')
-    | Přihláška do       | Název akce/závodu        | ORIS ID
-    | :----------------- |:------------- |:------------- |
-    @foreach ($eventFirstDateEnd as $firstDate)
-        | {{ Carbon::parse($firstDate->entry_date_1)->format('d.m.Y - H:i') }}  | {{$firstDate->name }}<br>@if(EmptyType::stringNotEmpty($firstDate->alt_name)) {{Str::limit($firstDate->alt_name, 35)}}@endif | @if($firstDate->oris_id !== null)[{{$firstDate->oris_id }}](https://oris.orientacnisporty.cz/Zavod?id={{$firstDate->oris_id}})@endif  |
-    @endforeach
+| Přihláška do       | Datum akce         | Název akce/závodu   |
+| :----------------- |:------------------ |:------------------ |
+@foreach ($eventFirstDateEnd as $firstDate)
+| {{ Carbon::parse($firstDate->entry_date_1)->format('d.m.Y - H:i') }} | @if(EmptyType::stringNotEmpty($firstDate->alt_name)){{ Carbon::parse($firstDate->date)->format('d.m.Y') }}@endif | @if($firstDate->oris_id !== null)[{{$firstDate->name}}](https://oris.orientacnisporty.cz/Zavod?id={{$firstDate->oris_id}})@endif<br>@if(EmptyType::stringNotEmpty($firstDate->alt_name)){{Str::limit($firstDate->alt_name, 35)}}@endif | @if($firstDate->oris_id !== null)[{{$firstDate->oris_id }}](https://oris.orientacnisporty.cz/Zavod?id={{$firstDate->oris_id}})@endif  |
+@endforeach
 @endcomponent
 @endif
 
@@ -43,11 +43,12 @@ Závody u kterých končí **druhý termín** přihlášek.
 @endcomponent
 
 @component('mail::table')
-    | Přihláška do       | Název akce/závodu        | ORIS ID
-    | :----------------- |:------------- |:------------- |
-    @foreach ($eventSecondDateEnd as $secondDate)
-        | {{ Carbon::parse($secondDate->entry_date_2)->format('d.m.Y - H:i') }}  | {{$secondDate->name }}<br>@if(EmptyType::stringNotEmpty($secondDate->alt_name)) {{Str::limit($secondDate->alt_name, 35)}}@endif | @if($secondDate->oris_id !== null)[{{$secondDate->oris_id }}](https://oris.orientacnisporty.cz/Zavod?id={{$secondDate->oris_id}})@endif  |
-    @endforeach
+| Přihláška do       | Datum akce         | Název akce/závodu   |
+| :----------------- |:------------------ |:------------------ |
+@foreach ($eventSecondDateEnd as $secondDate)
+| {{ Carbon::parse($secondDate->entry_date_2)->format('d.m.Y - H:i') }} | @if(EmptyType::stringNotEmpty($secondDate->alt_name)){{ Carbon::parse($secondDate->date)->format('d.m.Y') }}@endif | @if($secondDate->oris_id !== null) [{{$secondDate->name}}](https://oris.orientacnisporty.cz/Zavod?id={{$secondDate->oris_id}})@endif<br>@if(EmptyType::stringNotEmpty($secondDate->alt_name)){{Str::limit($secondDate->alt_name, 35)}}@endif | @if($secondDate->oris_id !== null)[{{$secondDate->oris_id }}](https://oris.orientacnisporty.cz/Zavod?id={{$secondDate->oris_id}})@endif  |
+@endforeach
+
 @endcomponent
 @endif
 
@@ -60,11 +61,11 @@ Závody u kterých končí **třetí termín** přihlášek.
 @endcomponent
 
 @component('mail::table')
-    | Přihláška do       | Název akce/závodu        | ORIS ID
-    | :----------------- |:------------- |:------------- |
-    @foreach ($eventThirdDateEnd as $thirdDate)
-        | {{ Carbon::parse($thirdDate->entry_date_3)->format('d.m.Y - H:i') }}  | {{$thirdDate->name }}<br>@if(EmptyType::stringNotEmpty($thirdDate->alt_name)) {{Str::limit($thirdDate->alt_name, 35)}}@endif | @if($thirdDate->oris_id !== null)[{{$thirdDate->oris_id }}](https://oris.orientacnisporty.cz/Zavod?id={{$thirdDate->oris_id}})@endif  |
-    @endforeach
+| Přihláška do       | Datum akce         | Název akce/závodu   |
+| :----------------- |:------------------ |:------------------ |
+@foreach ($eventThirdDateEnd as $thirdDate)
+| {{ Carbon::parse($thirdDate->entry_date_3)->format('d.m.Y - H:i') }} | @if(EmptyType::stringNotEmpty($thirdDate->alt_name)) {{ Carbon::parse($thirdDate->date)->format('d.m.Y') }}@endif | @if($thirdDate->oris_id !== null) [{{$thirdDate->name}}](https://oris.orientacnisporty.cz/Zavod?id={{$thirdDate->oris_id}})@endif<br>@if(EmptyType::stringNotEmpty($thirdDate->alt_name)){{Str::limit($thirdDate->alt_name, 35)}}@endif | @if($thirdDate->oris_id !== null)[{{$thirdDate->oris_id }}](https://oris.orientacnisporty.cz/Zavod?id={{$thirdDate->oris_id}})@endif  |
+@endforeach
 @endcomponent
 @endif
 
